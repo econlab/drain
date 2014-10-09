0e064896e14f0cc875b22a7d1ccb782c33c6ed20
Miles Zimmerman <miles@zserver.org>
2014-10-08 17:40:51 -0700

    Syslog parser

--- index.js
+++ index.js
@@ -1,37 +1,18 @@
 /** Dependencies */
-var figaro  = require('figaro').parse();
-var redis   = require('redis').createClient();
-var express = require('express');
+var figaro    = require('figaro').parse();
+var redis     = require('redis').createClient();
+var express   = require('express');
+var LogParser = require('./lib/log_parser.js');
 
 
 /** Init */
 var app = express();
 
-/** Log parser */
-function parseLines(chunk) {
-  return chunk.split('\n')
-    .splice(0,-1)
-    .map(function(n) {
-      return n.replace(/^\d+ \<\d+\>\w /,'');
-    });
-}
 
-app.use(function(req, res, next) {
-  req.logLines = [];
-  req.setEncoding('utf8');
 
-  req.on('data', function(chunk) {
-    req.logLines = req.logLines.concat(parseLines(chunk));
-  });
-
-  req.on('end', function() {
-    next();
-  });
-});
-
-app.post('/test', function(req, res) {
+app.post('/test', LogParser.syslog, function(req, res) {
   console.log(req.get('Logplex-Msg-Count'));
-  console.log(req.logLines);
+  console.log(req.log_lines);
   res.status(200).end();
 });
 
