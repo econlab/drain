83d3a5caf163307dc8974e415f472196e1a94f33
Miles Zimmerman <miles@zserver.org>
2014-10-08 20:29:30 -0700

    console IO

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 var express    = require('express');
 var bodyParser = require('body-parser');
 var logParser  = require('./lib/logparser.js');
-
+var broker     = require('./lib/broker.js');
+var ConsoleIO  = require('./lib/io/console.js');
 
 /** Init */
+Broker.use(new ConsoleIO());
 var app = express();
 
 
@@ -13,7 +15,7 @@ var app = express();
  */
 app.post('/syslog/:session', logParser.syslog, function(req, res) {
   req.loglines.forEach(function(line) {
-    redis.publish('syslogs', msg.syslog(req.param('session'), line));
+    broker.syslog(req.param('session'), line);
   });
 
   // OK
@@ -25,7 +27,7 @@ app.post('/syslog/:session', logParser.syslog, function(req, res) {
  * Records 
  */
 app.post('/session/:session', bodyParser.json, function(req, res) {
-
+  broker.record(req.param('session'), 'test.txt', 'sdfsdfsdfsdfsdf');
 
   // OK
   res.status(200).end();
